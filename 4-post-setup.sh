794cf7cc75275767b66407a4a99ecdac32abe926
Dan Lewis <dan@daniellewis.dev>
2021-08-16 23:15:37 +0100

    Add post setup script

---
+++ 4-post-setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
+hwclock --systohc
+sed -i '177s/.//' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "KEYMAP=uk" >> /etc/vconsole.conf
+echo "arch" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+echo root:password | chpasswd
+
+# You can add xorg to the installation packages, I usually add it at the DE or WM install script
+# You can remove the tlp package if you are installing on a desktop or vm
+
+pacman -S grub efibootmgr networkmanager network-manager-applet dosfstools base-devel linux-headers os-prober
+
+# pacman -S --noconfirm xf86-video-amdgpu
+# pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable NetworkManager
+
+useradd -m dan
+echo dan:password | chpasswd
+#usermod -aG libvirt ermanno
+
+echo "dan ALL=(ALL) ALL" >> /etc/sudoers.d/dan
+
+printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
