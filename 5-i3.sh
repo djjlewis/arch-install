632ba403bf6f75de409af98942745842cb0ec684
Dan Lewis <dan@daniellewis.dev>
2021-08-16 23:48:14 +0100

    more x stuff

--- 5-i3.sh
+++ 5-i3.sh
@@ -1,1 +1,1 @@
-pacman -S --noconfirm i3 alacritty dmenu dunst feh firefox flameshot gnome-keyring htop mpv network-manager-applet picom pavucontrol redshift starship terminus-font stow thunar tmux ttf-inconsolata xclip xdg-user-dirs xorg-xinit 
+pacman -S --noconfirm xorg-server xorg-xinit xorg-apps xterm mesa xf86-video-vesa xf86-video-vmware xclip xdg-user-dirs xorg-xinit i3 alacritty dmenu dunst feh firefox flameshot gnome-keyring htop mpv network-manager-applet picom pavucontrol redshift starship terminus-font stow thunar tmux ttf-inconsolata zsh
