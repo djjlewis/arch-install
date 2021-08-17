351097a62fbe1bb578bca755be9da6dcac287e87
Dan Lewis <dan@daniellewis.dev>
2021-08-17 22:15:34 +0100

    more scripts

--- 4-post-setup.sh
+++ 4-post-setup.sh
@@ -31,4 +31,8 @@ echo dan:password | chpasswd
 
 echo "dan ALL=(ALL) ALL" >> /etc/sudoers.d/dan
 
+# Install vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
