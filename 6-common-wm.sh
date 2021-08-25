267622d3f5baf036927adcdefdc5c29a07af521b
Dan Lewis <dan@daniellewis.dev>
2021-08-25 19:51:38 +0100

    add dwt wallpaper git

--- 6-common-wm.sh
+++ 6-common-wm.sh
@@ -1,2 +1,4 @@
 sudo systemctl enable cronie.service
 (crontab -l 2>/dev/null; echo "*/5 * * * * DISPLAY=:0 /home/dan/.local/bin/wallpaper") | crontab -
+
+git clone https://gitlab.com/dwt1/wallpapers.git ~/Pictures/wallpaper
