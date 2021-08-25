79c4197c40e2099ef0aadbfe1ff166dc18ab3b41
Dan Lewis <dan@daniellewis.dev>
2021-08-25 20:56:34 +0100

    Add snapper config

---
+++ 7-snapper.sh
@@ -0,0 +1,8 @@
+sudo pacman -S --noconfirm snapper
+#sudo umount /.snapshots
+#sudo rm -rf /.snapshots
+
+sudo snapper -c root create-config /
+
+# sudo chmod a+rx /.snapshots
+# echo "Edit users in /etc/snapper/configs/root"
