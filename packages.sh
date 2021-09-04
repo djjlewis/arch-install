7b109a36cd35179e4eb31dd465d5fe707d4a7143
Dan Lewis <dan@daniellewis.dev>
2021-09-04 20:53:33 +0100

    snapper changes

---
+++ packages.sh
@@ -0,0 +1,1 @@
+yay -S bitwarden diff-so-fancy
--- snapper.sh
+++ snapper.sh
@@ -1,9 +1,14 @@
+set -e
+
 sudo pacman -S --noconfirm snapper
 sudo umount /.snapshots
 sudo rm -rf /.snapshots
 
 sudo snapper -c root create-config /
 
-yay -S snap-pac-grub snapper-gui
-# sudo chmod a+rx /.snapshots
-# echo "Edit users in /etc/snapper/configs/root"
+sudo chmod a+rx /.snapshots
+echo "Edit users in /etc/snapper/configs/root"
+
+sudo systemctl enable grub-btrfs.path
+
+#yay -S snap-pac-grub snapper-gui
